e883560285668ad61e23f686af158bf549fd9560
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-01 19:05:45 +0200

    cleanup commments

--- aidar.py
+++ aidar.py
@@ -1,11 +1,6 @@
-from io import StringIO
 
-import pandas as pd
 import requests as requests
 import streamlit as st
-
-from PIL import Image
-
 
 st.title("AIDAR - Disaster relief management")
 
@@ -18,21 +13,3 @@ if st.button("Style Transfer"):
         with open("response.jpg", "wb") as f:
             f.write(res.content)
         st.image("response.jpg", width=800)
-        #st.image(image, width=500)
-
-#if image is not None:
-    # To read file as bytes:
-#    bytes_data = image.getvalue()
-#    st.write(bytes_data)
-
-    # To convert to a string based IO:
-#    stringio = StringIO(image.getvalue().decode("utf-8"))
-#    st.write(stringio)
-
-    # To read file as string:
-#    string_data = stringio.read()
-#    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-#    dataframe = pd.read_csv(image)
-#    st.write(dataframe)
